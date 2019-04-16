84e1dde8a4ea866f0471438a93e7a69ee317ea40
lcossp00 <lcossp00@estudiantes.unileon.es>
2019-04-16 18:08:45 +0200

    P6 Acabada

--- P6_Sustit_Monoalfabetica/src/Monoalfabetica.java
+++ P6_Sustit_Monoalfabetica/src/Monoalfabetica.java
@@ -1,13 +1,12 @@
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Monoalfabetica {
 	//Monoalfabetica
 	static String alf;
 	static String texto_01;
 	static String texto_02;
-	static ArrayList<Integer> textoNum;
-	static ArrayList<Integer> descodificadoNUM;
-	static ArrayList<Character> descodificadoString;
+	static String texto_03;
 	
 	//Vigenere
 	static String clave;
@@ -18,17 +17,31 @@ public class Monoalfabetica {
 		
 		alf ="aábcdeéfghiíjklmnñoópqrstuúvwxyzAÁBCDEÉFGHIÍJKLMNÑOÓPQRSTUÚVWXYZ0123456789 ,.:-()";
 		texto_01 = "NBHQÑÍHBHQúÍahYBWQ(BñAhQHtÑAayaÍQaAQÑtB:ÚtyAhQÍ9hBQQwBWAhBQrdQHyWQAwñBhMÍdQHtÑAayAhÍWQBHYDQPBWyA:dQA:QQ)ÍÑ(tA:ÍdQaAHaAQA:QmÍWaÍQaAQHtHQÍWÑAQBíÍHdQ:BwAW9BñBQQA:QÑthHÍQaAQ:ÍHQBÑÍW9AÑywyAW9ÍHdQBtWÚtAQ:ÍQBÑB9BhBQQÑÍwÍQtWBQhAB:yaBaQyWAfy9Bñ:AQrQmB9B:D";
-		textoNum = new ArrayList<Integer>();
-		descodificadoNUM = new ArrayList<Integer>();
-		descodificadoString = new ArrayList<Character>();
+		
+		texto_02 = "p5nty)s.úmfgá-r)3qvvcEhU3te-rfmiEd mdv  bmól45m-h( gDcl(8í-p,s.ÍCóÚgh-úoeuvAAÚ5md(on.móó49í-úoe.zwyé0.d(  uMcB14mha3PfJwfdám-óí sMmf1U)o(3ahvkÉ12 )a5";
+		
+		texto_03="C3HUu()cQtoEZsÓxu:S IilauKhLgsY3ÁtGáXf";
 		
 		clave = "10 de abril";
-		texto_02 = "p5nty)s.úmfgá-r)3qvvcEhU3te-rfmiEd mdv  bmól45m-h( gDcl(8í-p,s.ÍCóÚgh-úoeuvAAÚ5md(on.móó49í-úoe.zwyé0.d(  uMcB14mha3PfJwfdám-óí sMmf1U)o(3ahvkÉ12 )a5";
-
 		
-		monoalfabetica();
+		//EJERCICIO 1
+		monoalfabetica(texto_01);
 		
-		//vigenere();
+		System.out.println("");
+		
+		//EJERCICIO 2
+		vigenere(texto_02);
+		
+		System.out.println("");
+		
+		//EJERCICIO 3
+		ArrayList<Character> result = vigenere(texto_03);
+		String primerTexto = result.get(0)+"";
+		for(int i =1; i<result.size(); i++) {
+			primerTexto = primerTexto + result.get(i)+"";
+		}
+		System.out.println("");
+		monoalfabetica(primerTexto);
 		
 		/*
 		//Imprimir array list
@@ -38,21 +51,15 @@ public class Monoalfabetica {
 		}
 		 */
 		
-		
 	}
 	
-	public static void monoalfabetica() {
-		//Obtener los numeros en el alfabeto
-		/*for(int i=0; i<texto.length(); i++) {
-			for(int j=0; j<alf.length();j++) {
-				if(texto.charAt(i) == alf.charAt(j)) {
-					textoNum.add(j);
-					
-				}
-				
-			}		
-		}*/
-		textoNum = posiciones(texto_01);
+	public static ArrayList<Character> monoalfabetica(String texto) {
+		ArrayList<Integer> textoNum = new ArrayList<Integer>();
+		ArrayList<Integer> descodificadoNUM = new ArrayList<Integer>();
+		ArrayList<Character> descodificadoString =  new ArrayList<Character>();
+		
+		textoNum = posiciones(texto);
+		
 		//Descodificamos
 		for(int k=0; k<textoNum.size();k++) {
 			descodificadoNUM.add(((textoNum.get(k) *38) +4)%81);
@@ -60,17 +67,23 @@ public class Monoalfabetica {
 		for(int l=0; l<descodificadoNUM.size(); l++) {
 			descodificadoString.add(alf.charAt(descodificadoNUM.get(l)));
 		}
+		for (int p = 0; p < descodificadoString.size(); p++) {
+			System.out.print(descodificadoString.get(p));
+
+		}
+		return descodificadoString;
+		
 	}
-	public static void vigenere() {
+	public static ArrayList<Character> vigenere(String texto) {
 		ArrayList<Integer> numericoClave = posiciones(clave);
-		ArrayList<Integer> numericoCifrado = posiciones(texto_02);
+		ArrayList<Integer> numericoCifrado = posiciones(texto);
 		ArrayList<Integer> codClave = new ArrayList<Integer>();
 		ArrayList<Integer> resta = new ArrayList<Integer>();
 		ArrayList<Character> decodificacion;
 		//codClave es el array list que contiene tantas veces repetida la clave como longitud tenga el cifrado
 		
 		int index=0;
-		for(int i=0; i<texto_02.length(); i++) {
+		for(int i=0; i<texto.length(); i++) {
 			index = i % numericoClave.size();
 			codClave.add(numericoClave.get(index));
 			
@@ -95,6 +108,7 @@ public class Monoalfabetica {
 			System.out.print(decodificacion.get(l));
 
 		}
+		return decodificacion;
 	
 	}
 	public static ArrayList<Integer> posiciones(String texto) {
