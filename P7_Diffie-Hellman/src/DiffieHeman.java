3a9a809adb1666397bbd9e8afa581a1cd113bf17
acasag03 <acasag03@estudiantes.unileon.es>
2019-05-15 10:33:09 +0200

    Add files via upload

--- P7_Diffie-Hellman/src/DiffieHeman.java
+++ P7_Diffie-Hellman/src/DiffieHeman.java
@@ -4,20 +4,23 @@ import java.util.ArrayList;
 public class DiffieHeman {
 
 
+	public static String alf = "aábcdeéfghiíjklmnñoópqrstuúvwxyzAÁBCDEÉFGHIÍJKLMNÑOÓPQRSTUÚVWXYZ0123456789 ,.:-()";
 
 	public static void main(String[] args) {
-		
-		//String  prueba = calculaBase("14","2");
+		BigInteger num = new BigInteger("14");
+		BigInteger base = new BigInteger("2");
+		//BigInteger  prueba = calculaBase(num,base);
 		//System.out.print(prueba);
-		BigInteger a = potenciacionModular("3","14","11");
-		System.out.println(a);
-		BigInteger Ejercicio1 = potenciacionModular("2424242424","4444444444","201920192019");
-		System.out.println(Ejercicio1);
+		String _num = "123456789123456789";
+		String _base = "81";
+		numeroTexto(_num,_base);
+		String[] hola = alfabetoEnteros("24 de Abril");
 	}
 	
 	
 	//Calcula el modulo de una potencia
-	public static BigInteger potenciacionModular(String base, String exp, String mod) {
+	public static BigInteger potenciacionModular(String base, String exp, String mod)
+	{
 		
 		ArrayList<BigInteger> a = new ArrayList<BigInteger>();
 		ArrayList<BigInteger> b = new ArrayList<BigInteger>();
@@ -50,10 +53,10 @@ public class DiffieHeman {
 		}
 		return m.get(m.size()-1);
 	}
-
 	
-	//Calcula un numero segun la base
-	public static String  calculaBase(String _numero, String _base) {
+	//Calcula un numero segun la base-------------Usar BigInteger
+	public static String  calculaBase(String _numero, String _base) 
+	{
 		String resto;
 		BigInteger numero = new BigInteger(_numero);
 		BigInteger base = new BigInteger(_base);
@@ -75,17 +78,80 @@ public class DiffieHeman {
 		return resto;
 	}
 	//Da la vuelta al string que recibe, Original=0111 Result=1110
-	public static String reverse(String cadena) {
+	public static String reverse(String cadena)
+	{
 		String cadenaInvertida = "";
 		for (int x=cadena.length()-1;x>=0;x--)
 			cadenaInvertida = cadenaInvertida + cadena.charAt(x);
 		return cadenaInvertida;
 	}
 	//
-	public static void codTexto() {
+	public static void codTexto()
+	{
 		
 	}
-	public static void codViceversa() {
+	public static void codViceversa()
+	{
 		
+	}
+	
+	public static String numeroTexto(String numero, String base)
+	{
+		//CAMBIAR EL METODO CALCULABASE PARA QUE DEVUELVA 2 CIFRAS y tiene que devolver un string
+		String cadenaResuelta = calculaBase(numero , base);
+		
+		for(int i = 0;i < cadenaResuelta.length(); i++)
+		{
+			int cadenaInt = Character.getNumericValue(cadenaResuelta.charAt(i));
+			char variable = alf.charAt(cadenaInt);
+			System.out.println(cadenaInt + "  " +variable);
+		}
+		return null;
+
+	
+	}
+	
+	public static void textoNumero(int[] args, BigInteger base)
+	{
+		BigInteger suma = new BigInteger("0");
+		
+		for(int i = 0; i < args.length; i++)
+		{
+			String num = String.valueOf(args[i]);
+			BigInteger n = new BigInteger(num);
+			//n = n.multiply(potenciacionModular(base , args.length - i));
+			suma = suma.add(n);
+		}
+	}
+	
+	public static String[] alfabetoEnteros(String arg)
+	{
+		String[] cadenaNumeros = new String[arg.length()];
+		
+		for(int i=0; i < arg.length();i++)
+		{
+			for(int j=0; j < alf.length();j++)
+			{
+				if(arg.charAt(i) == (alf.charAt(j)))
+				{
+					cadenaNumeros[i] = String.valueOf(j);
+					
+				}
+			}
+		}
+		
+		return cadenaNumeros;
+	}
+	
+	public static void Diffie(String A, String b, String p)
+	{
+		//Pasamos las cadenas de texto a numero
+		//Para crear ñla clave publica debemos hacer g elevado a a*b modulo p
+		//Donde a es el numeor de Alicia y b es el numeor de Benito , p y g son datos que nos dan
+		//Para sacer el numero Alicia, g elevado a a modulo de p
+		//Para sacer el numeor Benito, g elevado a b modulo de p
+		
+		String n = numeroTexto(A, p);
+		BigInteger resultado = potenciacionModular(p,b,n);
 	}
 }
